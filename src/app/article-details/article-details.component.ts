acdc3509f4b09c3d49f86d1da7a6dc5a582f4230
anastasiosPou <tasos.poursaitidis.4@gmail.com>
2024-06-19 11:28:57 +0300

    Use the Articles service to fetch articles

--- src/app/article-details/article-details.component.ts
+++ src/app/article-details/article-details.component.ts
@@ -1,18 +1,26 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ArticlesService } from '../articles.service';
+import { Article } from '../article';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-article-details',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './article-details.component.html',
   styleUrl: './article-details.component.scss'
 })
 export class ArticleDetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
-  articleID: Number
+  articlesService: ArticlesService = inject(ArticlesService);
+  article: Article | undefined;
+
   constructor() {
     const articleID = Number(this.route.snapshot.params["id"]);
-    this.articleID = articleID;
+    this.articlesService.getArticleBy(articleID)
+    .then((article: Article | undefined) => {
+      this.article = article;
+    });
   }
 }
